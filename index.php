a26c74a0f6f3cda74807b7f889a8c8ba0f8b5300
Jérémie Litzler <webdev.jl@gmail.com>
2015-08-27 07:06:50 +0200

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Offline Upload</title>
+    <script type="application/javascript" src="resize.js"></script>
+  </head>
+  <body>
+    <input type="file" id="f" multiple>
+  </body>
+</html>
---
+++ resize.js
@@ -0,0 +1,23 @@
+var h = function (e) {
+  var fr = new FileReader();
+  fr.onload = function (e) {
+    var img = new Image();
+    img.onload = function () {
+      var MAXWidthHeight = 1024;
+      var r = MAXWidthHeight / Math.max(this.width, this.height),
+              w = Math.round(this.width * r),
+              h = Math.round(this.height * r),
+              c = document.createElement("canvas");
+      c.width = w;
+      c.height = h;
+      c.getContext("2d").drawImage(this, 0, 0, w, h);
+      this.src = c.toDataURL();
+      document.body.appendChild(this);
+    }
+    img.src = e.target.result;
+  }
+  fr.readAsDataURL(e.target.files[0]);
+}
+window.onload = function () {
+  document.getElementById('f').addEventListener('change', h, false);
+}
